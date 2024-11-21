d3199077de655644e983a53a49d960cc0e385ca8
Dr-AddictStein <dr.addictstein@gmail.com>
2024-11-22 01:20:51 +0600

    header-fixd

--- boctrust-loanapp-frontend/src/components/dashboard/topnavbar/TopNavber.jsx
+++ boctrust-loanapp-frontend/src/components/dashboard/topnavbar/TopNavber.jsx
@@ -1,19 +1,29 @@
 import PropTypes from "prop-types";
 import "./TopNavbar.css";
+import { useDispatch } from "react-redux";
+import { handleLogout } from "../../../services/logout";
 
-const TopNavber = ({ title, user="Femi Akinwade" }) => {
+const TopNavber = ({ title, user = "Femi Akinwade" }) => {
+
+  // handle logout
+  const dispatch = useDispatch();
+  const logoutUser = async () => {
+    try {
+      await handleLogout();
+      dispatch(logoutUser());
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <div className="Inline mb-4">
       <div>
         <h4 id="Title">{title}</h4>
       </div>
-      <div className="Inline Profile">
-        <img src="/images/notificationicon.png" alt="alert" />
-        <div className="Inline UserCard">
-          <p>{user}</p>
-          <img src="/images/smallavater.png" alt="user" />
-        </div>
-      </div>
+      <button onClick={logoutUser} className="" style={{ backgroundColor: "#145098", marginRight: "10px", color: "white", fontSize: "23px", borderRadius: "5px", paddingLeft: "20px", paddingRight: "20px", paddingTop: "6px", paddingBottom: "6px" }}>Log Out</button>
     </div>
   );
 };
