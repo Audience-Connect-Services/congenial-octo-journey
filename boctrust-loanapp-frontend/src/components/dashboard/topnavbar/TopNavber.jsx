342c326b034e86983ff2968b06a868d3b1da7ce9
Dr-AddictStein <dr.addictstein@gmail.com>
2024-11-22 01:41:33 +0600

    header-fixd

--- boctrust-loanapp-frontend/src/components/dashboard/topnavbar/TopNavber.jsx
+++ boctrust-loanapp-frontend/src/components/dashboard/topnavbar/TopNavber.jsx
@@ -1,29 +1,44 @@
 import PropTypes from "prop-types";
 import "./TopNavbar.css";
 import { useDispatch } from "react-redux";
-import { handleLogout } from "../../../services/logout";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { performLogout } from "../../../redux/reducers/adminAuthReducer";
 
 const TopNavber = ({ title, user = "Femi Akinwade" }) => {
-
-  // handle logout
   const dispatch = useDispatch();
-  const logoutUser = async () => {
+  const navigate = useNavigate(); // Initialize navigate
+
+  const logoutUserHandler = async () => {
     try {
-      await handleLogout();
-      dispatch(logoutUser());
-      navigate("/login");
+      await dispatch(performLogout()); // Dispatch the thunk
+      navigate("/login"); // Redirect after successful logout
     } catch (error) {
-      console.log(error);
+      console.error("Logout error:", error);
     }
   };
-
 
   return (
     <div className="Inline mb-4">
       <div>
         <h4 id="Title">{title}</h4>
       </div>
-      <button onClick={logoutUser} className="" style={{ backgroundColor: "#145098", marginRight: "10px", color: "white", fontSize: "23px", borderRadius: "5px", paddingLeft: "20px", paddingRight: "20px", paddingTop: "6px", paddingBottom: "6px" }}>Log Out</button>
+      <button
+        onClick={logoutUserHandler}
+        className=""
+        style={{
+          backgroundColor: "#145098",
+          marginRight: "10px",
+          color: "white",
+          fontSize: "23px",
+          borderRadius: "5px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          paddingTop: "6px",
+          paddingBottom: "6px",
+        }}
+      >
+        Log Out
+      </button>
     </div>
   );
 };
